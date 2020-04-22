0a98ca4c6bd3244d06062d23901157c5598c0dfd
森近　楓 <kaede.morichika@onlineconsultant.jp>
2020-04-22 13:41:41 +0900

    DB接続設定

---
+++ OCTech/github/sample.php
@@ -0,0 +1,14 @@
+<?php
+// MySQL接続のための定数設定
+$dsn = 'mysql:dbname=dbhoge;host=localhost';
+$user = 'root';
+$psw = 'hogehoge';
+
+// DB接続
+try {
+    $db = new PDO($dsn, $user, $psw);
+} catch (PDOException $e) {
+    print 'DB Connection failed:'.$e->getMessage();
+    die;
+}
+?>
