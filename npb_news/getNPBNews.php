9cf3a709ad47baa9ae6fc5d8c725ec5c48ba34d7
kaede_morichika <kaede.morichika@onlineconsultant.jp>
2020-09-09 19:34:00 +0900

    不要そうなものを削除

--- npb_news/getNPBNews.php
+++ npb_news/getNPBNews.php
@@ -2,24 +2,17 @@
 try {
     $url = 'https://www.nikkansports.com/baseball/professional/atom.xml';
 
-    header('Access-Control-Allow-Origin: https://www.nikkansports.com/baseball/professional/atom.xml');
-    header('Access-Control-Allow-Headers: "X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept"');
-
     $ch = curl_init();
-
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
 
     $response = curl_exec($ch);
     $xml = simplexml_load_string($response);
-
     curl_close($ch);
 
     $articles = [];
 
     foreach ($xml->entry as $article) {
-
         $contents = [];
         $contents['title'] = $article->title;
         $contents['url'] = $article->id;
