1c026778ee709ffcb18c5f6e70d62826aee9a47d
kaede_morichika <kaede.morichika@onlineconsultant.jp>
2020-12-25 11:45:10 +0900

    autoload対応

--- teishoku_order/index.php
+++ teishoku_order/index.php
@@ -3,9 +3,6 @@ require_once(__DIR__ . '/config/const.php');
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/utils/auth_utils.php');
 
-require_once(__DIR__ . '/class/auth/SingletonPDO.php');
-require_once(__DIR__ . '/class/auth/User.php');
-
 use auth\User;
 
 if (!empty($_GET['action'])) {
